d803f403b6fd5d7dea04cc74b8db738ca9973db7
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-08-12 06:31:45 +0000

    Deploying to docs from @ books-are-next/kratochvil-nocni-tango-aneb-roman-jednoho-leta-z-konce-stoleti@112c8d5bf31f75ccc23708251f876780da6f2734 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-6e1bc3e';
+const CACHE = 'cache-112c8d5';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */

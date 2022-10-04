99e7eb91c9fe4a9e6765ba24ee10258bade0fea5
jan-martinek <jan-martinek@users.noreply.github.com>
2022-10-04 23:12:26 +0000

    Deploying to docs from @ books-are-next/kratochvil-nocni-tango-aneb-roman-jednoho-leta-z-konce-stoleti@f91db1e70396a9387a622c0a45de29fd54fe339f 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-ab1e2d6';
+const CACHE = 'cache-f91db1e';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
